982d89134b27315dfd9cc3845dcb94d6d92f36f2
JiangXuanke <15223370366@163.com>
2018-08-17 14:11:42 +0800

    Initial Commit

---
+++ lesson2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lesson2
+//
+//  Created by JiangXuanke on 2018/8/17.
+//  Copyright © 2018年 JiangXuanke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lesson2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lesson2
+//
+//  Created by JiangXuanke on 2018/8/17.
+//  Copyright © 2018年 JiangXuanke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
