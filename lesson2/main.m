e0585dfc9cdac0944a305a026731360fb0bb27a2
JiangXuanke <15223370366@163.com>
2018-08-18 20:47:48 +0800

    the simple insert sort

--- lesson2/main.m
+++ lesson2/main.m
@@ -8,9 +8,15 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
+#import "SimpleSort.h"
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        int a[10] = {23,55,2,123,66,3,45,77,89,4};
+        SimpleSort *simple = [[SimpleSort alloc] init];
+        int* result = [simple simpleSort:a];
+        [simple printResult:result];
+        //return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
+    return 0;
 }
